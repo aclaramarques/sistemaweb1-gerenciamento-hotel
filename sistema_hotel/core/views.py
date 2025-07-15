014ea8dafee34700c8a884e739d0ff120c42978c
Paulo José <paulo.aluno58@gmail.com>
2025-07-15 14:21:30 -0300

    Correção do cálculo das diárias

--- sistema_hotel/core/views.py
+++ sistema_hotel/core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from quartos.models import Quarto
 from clientes.models import Hospede
+from reservas.models import Reserva
+
 
 def listar_tudo(request):
     quartos = Quarto.objects.all()
     hospedes = Hospede.objects.all()
-    return render(request, 'index.html', {'quartos': quartos, 'hospedes': hospedes})
+    reservas = Reserva.objects.all()
+    return render(request, 'index.html', {'quartos': quartos, 'hospedes': hospedes, 'reservas' : reservas})
