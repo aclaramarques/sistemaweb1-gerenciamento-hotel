a09299c495948276d252e4d03e2b487a311ffb44
andsonqueiroz <mrpewceful@gmail.com>
2025-07-15 13:10:25 -0300

    Estilizei os formulários de cadastro

--- sistema_hotel/quartos/forms.py
+++ sistema_hotel/quartos/forms.py
@@ -5,3 +5,9 @@ class QuartoForm(forms.ModelForm):
     class Meta:
         model = Quarto
         fields = ['numero', 'tipo', 'capacidade', 'preco_diaria']
+        widgets = {
+            'numero': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tipo': forms.TextInput(attrs={'class': 'form-control'}),
+            'capacidade': forms.NumberInput(attrs={'class': 'form-control'}),
+            'preco_diaria': forms.NumberInput(attrs={'class': 'form-control'})
+        }
