660d5b7f80d480fd0ac33c0f483d07dd28bb64f2
AnaClaraAMarques <anaclaraamarques890@gmail.com>
2025-07-15 22:16:02 -0300

    ultimas alterações

--- sistema_hotel/pagamentos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('pagamentos', views.listar_quartos, name='listar_pagamentos'),
-    path('novo-pagamento/', views.cadastrar_quarto, name='cadastrar_pagamento'),
-    path('editar-pagamento/<int:id>', views.editar_quarto, name='editar_pagamento'),
-    path('deletar-pagamento/<int:id>', views.deletar_quarto, name='deletar_pagamento'),
-]
--- sistema_hotel/servicos_adicionais/models.py
+++ sistema_hotel/servicos_adicionais/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Servicos_adicionais(models.Model):
     tipo = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
-    valor = models.IntegerField()
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return self.tipo
+        return self.valor
